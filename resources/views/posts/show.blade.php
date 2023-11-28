d24bfe74a9f237cac38afd4ff8ef26ed4222ba61
tomomichi553 <yoshidatomomichi373@gmail.com>
2023-11-28 22:37:40 +0900

    編集機能の追加

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -19,6 +19,7 @@
             <p>{{$post->body}}</p>
         </div>
     </div>
+    <div class="edit"><a href="/posts/{{$post->id}}/edit">edit</a></div>
     <div class="footeer">
         <a href="/">戻る</a>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/posts/create',[PostController::class,'create']);
 
 Route::get('/posts/{post}',[PostController::class,'show']);
 
+Route::get('posts/{post}/edit',[PostController::class,'edit']);
+
+Route::put('posts/{post}',[PostController::class,'update']);
+
 Route::post('/posts',[PostController::class,'store']);
 
 
