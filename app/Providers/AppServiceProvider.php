20e074dfe46cd2b5cdb0de691e9274846b3f5dbb
tomomichi553 <yoshidatomomichi373@gmail.com>
2023-12-20 13:05:23 +0900

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,5 +24,6 @@ class AppServiceProvider extends ServiceProvider
             Paginator::useBootstrap();
             \URL::forceScheme('https');
             $this->app['request']->server->set('HTTPS','on');
+            Schema::defaultStringLength(191);
     }
 }
